e861e799da86737b2925a0fd1357c728c4a3d338
sijiayangcamer <93570260+sijiayangcamer@users.noreply.github.com>
2026-01-21 06:12:56 +0000

    Deploying to gh-pages from @ sijiayangcamer/sijiayangcamer.github.io@719205b67f61559f0794b357355f7a68ad98dfba 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "sijiayangcamer/camer.github.io",
+    "data-repo": "sijiayangcamer/sijiayangcamer.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
